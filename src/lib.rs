dcb0367fe360e58bf77af4b261867ea393cdc05a
anukuga <anukuga02@gmail.com>
2024-03-30 11:22:46 +0100

    passed

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,30 @@
 #[derive(Debug)]
-pub struct HighScores;
+pub struct HighScores {
+     scores: Vec<u32>,
+}
 
 impl HighScores {
-    pub fn new(scores: &[u32]) -> Self {
-        unimplemented!("Construct a HighScores struct, given the scores: {scores:?}")
+     pub fn new(scores: &[u32]) -> Self {
+         HighScores {
+             scores: scores.to_vec(),
+         }
     }
 
-    pub fn scores(&self) -> &[u32] {
-        unimplemented!("Return all the scores as a slice")
+     pub fn scores(&self) -> &[u32] {
+         &self.scores
     }
 
-    pub fn latest(&self) -> Option<u32> {
-        unimplemented!("Return the latest (last) score")
+     pub fn latest(&self) -> Option<u32> {
+         self.scores.last().copied()
     }
 
-    pub fn personal_best(&self) -> Option<u32> {
-        unimplemented!("Return the highest score")
+     pub fn personal_best(&self) -> Option<u32> {
+         self.scores.iter().max().copied()
     }
 
-    pub fn personal_top_three(&self) -> Vec<u32> {
-        unimplemented!("Return 3 highest scores")
+     pub fn personal_top_three(&self) -> Vec<u32> {
+         let mut scores = self.scores.clone();
+         scores.sort_unstable();
+         scores.iter().rev().take(3).copied().collect()
     }
 }
